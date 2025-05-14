3c80133bbd24199f9ed5024c1c8aebaeb5c9d9c1
Isha Jain <75637232+ishaj72@users.noreply.github.com>
2025-05-14 11:03:58 +0530

    adding comments

--- yourFirstVariable.sh
+++ yourFirstVariable.sh
@@ -3,16 +3,16 @@ MY_SHELL="Scripting is fun"
 echo "Hello everyone $MY_SHELL"
 
 
-// Using variable for arithmatic calculation
+# Using variable for arithmatic calculation
 a = 2
 b = 3
 sum = $ ((a+b))
 echo "sum is $sum"
 
-// Using variable to provide default value if variables value is not given 
+# Using variable to provide default value if variables value is not given 
 echo "Value: ${my_val:-default_val}"
 
-//Using variable as string
+# Using variable as string
 echo”${MY_SHELL}ing”
 
 
