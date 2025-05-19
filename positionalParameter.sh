52a3f0762295da11243af08ac9a71a065ea552ed
Isha Jain <75637232+ishaj72@users.noreply.github.com>
2025-05-19 22:02:56 +0530

    Create positionalParameter.sh

---
+++ positionalParameter.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+echo "Hello everyone , I am $1" # argument1
+echo "Lets start leaarning $2" #argument 2
+echo "Total number of arguments are:  $#" # total argument
+echo "Arguments in string pattern:  $*" # argument in string
